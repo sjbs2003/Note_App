7581e7f6b80fe79faa36966a37b00087266c7f37
vicki <surajbarik2003@gmail.com>
2024-10-09 00:24:02 +0530

    created whole new Ui with, new incoming functionalities

--- app/src/main/java/com/example/noteapp/ui/screens/NoteListViewModel.kt
+++ app/src/main/java/com/example/noteapp/ui/screens/NoteListViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.noteapp.ui.screens
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.noteapp.data.room.NoteEntity
 import com.example.noteapp.data.room.NoteRepository
